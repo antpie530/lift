d84fa707d103178fd0b0822f3599f626aeee0f27
antpie530 <antpie530@gmail.com>
2024-08-26 23:59:22 +0000

    Add migrations to app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
+import { Text, View } from "react-native";
 import { Stack } from "expo-router";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import migrations from "@/drizzle/migrations";
+import { db } from "@/db/db";
 
 export default function RootLayout() {
+  const { error, success } = useMigrations(db, migrations)
+
+  if (error) {
+    return (
+      <View>
+        <Text>Database migrations error: {error.message}</Text>
+      </View>
+    )
+  }
+
+  if (!success) {
+    return (
+      <View>
+        <Text>Database loading...</Text>
+      </View>
+    )
+  }
   return (
     <Stack>
       <Stack.Screen name="index" />
