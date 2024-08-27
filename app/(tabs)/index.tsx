9adaab872114dfe2627bc6ce9122bda97f1b1f73
antpie530 <antpie530@gmail.com>
2024-08-27 00:02:45 +0000

    Add tabs:

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Traq() {
+    return (
+        <View>
+            <Text>Traq</Text>
+        </View>
+    )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout() {
   }
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
     </Stack>
   );
 }
