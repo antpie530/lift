251e132638f3bdd71a3a9043bdd39519b421333f
antpie530 <antpie530@gmail.com>
2024-10-01 18:10:36 +0000

    Update eslint config for imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,30 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["import", "prettier"],
   rules: {
-    "prettier/prettier": "error",
+    'import/order': [
+      'error',
+      {
+        'groups': [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent', 'index'],
+        ],
+        'newlines-between': 'always',
+        'alphabetize': { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    "prettier/prettier": ["error", {
+      tabWidth: 4
+    }]
+  },
+  settings: {
+    alias: {
+      map: [
+        ['@', './lift']
+      ]
+    }
   },
 };
--- components/Common/Buttons/AddSetButton/AddSetButton.tsx
+++ components/Common/Buttons/AddSetButton/AddSetButton.tsx
@@ -1,6 +1,6 @@
 import { TouchableOpacity, View } from "react-native";
 import Animated, { LinearTransition } from "react-native-reanimated";
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
 import { lightHaptic } from "@/utils/haptics/haptics";
 
@@ -9,22 +9,19 @@ import { AddSetButtonProps } from "./types";
 
 export default function AddSetButton({ addSet }: AddSetButtonProps) {
     return (
-        <Animated.View
-            layout={LinearTransition}
-            style={styles.container}
-        >
+        <Animated.View layout={LinearTransition} style={styles.container}>
             <TouchableOpacity
                 onPress={() => {
                     lightHaptic();
                     addSet();
                 }}
                 style={styles.buttonWrapper}
-            >  
+            >
                 <View style={{ paddingRight: 6 }}>
-                    <FontAwesome6 name="add" size={20} color="white"/>
+                    <FontAwesome6 name="add" size={20} color="white" />
                 </View>
                 <FontAwesome6 name="dumbbell" size={20} color="white" />
             </TouchableOpacity>
         </Animated.View>
-    )
+    );
 }
